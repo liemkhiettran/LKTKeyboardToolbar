80c6b2b62d78c4b0684cd576305fb4b6c5dfb804
liem-khiet tran <liemkhiet.tran@gmail.com>
2017-12-19 13:16:43 +0400

    Initial commit

---
+++ LKTKeyboardToolbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LKTKeyboardToolbar
+//
+//  Created by Liemkhiet Tran on 12/19/17.
+//  Copyright © 2017 liem-khiet tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
