8a3f1eca1d1fdb256f26d8aa0c2ece72d02ffadd
liem-khiet tran <liemkhiet.tran@gmail.com>
2017-12-19 15:09:02 +0400

    [master] : add example

--- LKTKeyboardToolbar/ViewController.swift
+++ LKTKeyboardToolbar/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    addTextField()
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +22,33 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  func addTextField() {
+    
+    let sampleTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 280, height: 44))
+    sampleTextField.placeholder = "Enter text here"
+    sampleTextField.font = UIFont.systemFont(ofSize: 15)
+    sampleTextField.borderStyle = UITextBorderStyle.roundedRect
+    sampleTextField.autocorrectionType = UITextAutocorrectionType.no
+    sampleTextField.keyboardType = UIKeyboardType.default
+    sampleTextField.returnKeyType = UIReturnKeyType.done
+    sampleTextField.clearButtonMode = UITextFieldViewMode.whileEditing;
+    sampleTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+    sampleTextField.delegate = self
+    self.view.addSubview(sampleTextField)
+    
+    addToolbarTo(sampleTextField)
+  }
+  
+  func addToolbarTo(_ textField: UITextField) {
+    
+    let toolbar = LKTKeyboardToolbar(leftButtonText: nil,
+                                     rightButtonText: "Done")
+    toolbar.rightAction = {() -> Void in
+      
+      self.view.endEditing(true)
+    }
+    
+    textField.inputAccessoryView = toolbar
+  }
 }
 
